3813df184e3b23219a72e772bb5556cf1cfb4d28
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-10-07 11:17:47 +0600

    init mongoose and user schema

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+// import authRoutes from './routes/authRoutes.js';
+// import taskRoutes from './routes/taskRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+// app.use('/auth', authRoutes);
+// app.use('/tasks', taskRoutes);
+
+
+
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.ennn1mj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const options = {};
+
+// Connection
+mongoose.connect(uri, options)
+    .then(() => console.log("Connection Successful"))
+    .catch(err => console.log("Connection error:", err));
+
+
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
