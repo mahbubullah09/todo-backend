4ea8509a6747b17b2586a4dc5d6095eda8b2dac7
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-10-07 11:26:50 +0600

    task schema

---
+++ Schemas/Task.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+
+const taskSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    status:{
+        type: String,
+        requeire: true,
+        enum: ['pending', 'ongoing', 'complete'],
+        default: "pending",
+
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    }
+
+})
+
+export default mongoose.model('Task', taskSchema)
